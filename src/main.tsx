26dd1a0ce1d14f351a6c17db7acb3bb1ed084f90
Riu <echo.adinfauzan@gmail.com>
2024-07-26 17:08:11 +0700

    Update main.tsx
    Signed-off-by: Riu <echo.adinfauzan@gmail.com>

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,8 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import AuthProvider from '@context/AuthProvider.tsx';
-
+  
 import './index.css';
-
 import App from './App.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
