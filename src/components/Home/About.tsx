a4e291ae63bdcb21df7921471226b3c0bd33408f
Riu <echo.adinfauzan@gmail.com>
2024-07-26 17:14:48 +0700

    Update About.tsx
    Signed-off-by: Riu <echo.adinfauzan@gmail.com>

--- src/components/Home/About.tsx
+++ src/components/Home/About.tsx
@@ -36,7 +36,7 @@ export default function About() {
         transition={{ duration: 0.7 }}
       >
         <p className={clsx('text-justify text-sm', 'md:max-w-[50%]')}>
-          <strog>RiuStudio</strog> adalah penyedia jasa pembuatan website yang fokus pada desain responsif, user-friendly, dan SEO-optimized.
+          <strong>RiuStudio</strong> adalah penyedia jasa pembuatan website yang fokus pada desain responsif, user-friendly, dan SEO-optimized.
           Kami membantu bisnis Anda untuk membangun kehadiran online yang kuat dan efektif.
         </p>
         <Image
